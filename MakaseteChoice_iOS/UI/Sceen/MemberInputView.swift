a3bffdd11c216390d3ef9aa33817078ac69c7b39
RyuseiNomi <kanontonto1010@gmail.com>
2020-05-09 18:47:06 +0900

    Create view files and revise Realm input setting in XCode

---
+++ MakaseteChoice_iOS/UI/Sceen/MemberInputView.swift
@@ -0,0 +1,16 @@
+//
+//  MemberInputView.swift
+//  MakaseteChoice_iOS
+//
+//  Created by 能美龍星 on 2020/05/09.
+//  Copyright © 2020 Ryusei Nomi. All rights reserved.
+//
+
+import SwiftUI
+
+struct MemberInputView: View {
+    
+    var body: some View {
+        Text("メンバー入力")
+    }
+}
---
+++ MakaseteChoice_iOS/UI/Sceen/ShuffleResultView.swift
@@ -0,0 +1,9 @@
+//
+//  ShuffleResultView.swift
+//  MakaseteChoice_iOS
+//
+//  Created by 能美龍星 on 2020/05/09.
+//  Copyright © 2020 Ryusei Nomi. All rights reserved.
+//
+
+import Foundation
