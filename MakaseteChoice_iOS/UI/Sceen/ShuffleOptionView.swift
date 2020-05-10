bbee4a9522f6dd5c4cf47c68cf81733f3a26fd57
RyuseiNomi <kanontonto1010@gmail.com>
2020-05-10 22:07:08 +0900

    add new icon and implement increment logic

--- MakaseteChoice_iOS/UI/Sceen/ShuffleOptionView.swift
+++ MakaseteChoice_iOS/UI/Sceen/ShuffleOptionView.swift
@@ -12,30 +12,59 @@ struct ShuffleOptionView: View {
     
     var member:[Member] = []
     @State private(set) var groupNum:Int = 1
+    @State private(set) var isUnderZero:Bool = false
+    @State private(set) var isOverMemberNum:Bool = false
     
     var body: some View {
         VStack() {
-            Text("グループ数")
-                .font(Font.custom("Helvetica-Light", size: 28))
-                .frame(maxWidth: .infinity, alignment: .leading)
-                .foregroundColor(Color(red: 105/255, green: 105/255, blue: 105/255))
             HStack() {
-                Button(action: {
-                    self.groupNum = self.groupNum - 1
-                })
-                {
-                    Image("Minus")
-                }
-                Text(String(self.groupNum))
+                Text("入力されたメンバー")
+                Text(String(member.count))
                     .foregroundColor(Color(red: 105/255, green: 105/255, blue: 105/255))
-                    .font(Font.custom("Helvetica-Light", size: 48))
-                Button(action: {
-                    self.groupNum = self.groupNum + 1
-                })
-                {
-                    Image("Plus")
+                    .font(Font.custom("Helvetica-Light", size: 30))
+                Text("人")
+            }
+            Spacer()
+            HStack() {
+                Image("Group")
+                VStack() {
+                    Text("グループ数")
+                        .font(Font.custom("Helvetica-Light", size: 28))
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .foregroundColor(Color(red: 105/255, green: 105/255, blue: 105/255))
+                    HStack() {
+                        Button(action: {
+                            if self.groupNum <= 1 {
+                                self.isUnderZero = true
+                                self.groupNum = 1
+                                return
+                            }
+                            self.groupNum = self.groupNum - 1
+                        }) {
+                            Image("Minus")
+                        }.alert(isPresented: $isUnderZero) {
+                            Alert(title: Text("1より少ない数は指定できません。"))
+                        }
+                        Text(String(self.groupNum))
+                            .foregroundColor(Color(red: 105/255, green: 105/255, blue: 105/255))
+                            .font(Font.custom("Helvetica-Light", size: 48))
+                            .padding(EdgeInsets(top: 5, leading: 20, bottom: 5, trailing:20))
+                        Button(action: {
+                            if self.groupNum > self.member.count-1 {
+                                self.isOverMemberNum = true
+                                return
+                            }
+                            self.groupNum = self.groupNum + 1
+                        })
+                        {
+                            Image("Plus")
+                        }.alert(isPresented: $isOverMemberNum) {
+                            Alert(title: Text("入力したメンバーより多い数は指定できません。"))
+                        }
+                    }
                 }
             }
+            .padding(EdgeInsets(top: 0, leading: 20, bottom: 0, trailing:20))
             Spacer()
             NavigationLink(destination: ShuffleResultView(members: self.member, groupNum: self.groupNum)) {
                 DecisionButton()
