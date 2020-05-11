96b37f46e527069c166d09a2d2873dd3f762db57
RyuseiNomi <kanontonto1010@gmail.com>
2020-05-11 23:00:04 +0900

    change button color

--- MakaseteChoice_iOS/UI/Component/DecisionButton.swift
+++ MakaseteChoice_iOS/UI/Component/DecisionButton.swift
@@ -9,19 +9,30 @@
 import SwiftUI
 
 struct DecisionButton: View {
+    
+    @EnvironmentObject public var appState: AppState
+    
     var body: some View {
         ZStack(alignment: .center) {
             HStack(alignment: .center) {
                 Text("次へ")
-                    .foregroundColor(Color(red: 105/255, green: 105/255, blue: 105/255))
+                    .foregroundColor(Color(red: 255/255, green: 255/255, blue: 255/255)) //white
                     .font(Font.custom("Helvetica-Light", size: 20))
                     .padding(EdgeInsets(top: 5, leading: 5, bottom: 5, trailing: 5))
                     .frame(alignment: .center)
             }
-            .frame(minWidth: 0, maxWidth: 100, alignment: .leading)
-            .background(Color(red: 255/255, green: 255/255, blue: 255/255))
+            .frame(minWidth: 0, maxWidth: 100, alignment: .center)
+            .background(self.getButtonBackgroundColor())
         }
         .cornerRadius(10)
         .shadow(color: Color(red: 173/255, green: 216/255, blue: 230/255), radius: 1, x: 0, y: 5) //lightblue
+    }
+    
+    public func getButtonBackgroundColor() -> Color {
+        var backgroundColor = Color(red: 169/255, green: 169/255, blue: 169/255) //darkgray
+        if self.appState.memberObject.isMemberIsOverTwo == true {
+            backgroundColor = Color(red: 255/255, green: 127/255, blue: 80/255) //coral
+        }
+        return backgroundColor
     }
 }
