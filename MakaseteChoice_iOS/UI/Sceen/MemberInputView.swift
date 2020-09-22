e6c6613db15cf08c20d900480af6324cb58e9abe
RyuseiNomi <kanontonto1010@gmail.com>
2020-09-23 00:20:25 +0900

    add Member addition button

--- MakaseteChoice_iOS/UI/Sceen/MemberInputView.swift
+++ MakaseteChoice_iOS/UI/Sceen/MemberInputView.swift
@@ -48,74 +48,37 @@ struct MemberInputView: View {
             ) { member in
                 MemberCell(member: member)
             }
-            Spacer()
             HStack() {
-                Spacer()
-                ZStack() {
-                    Button(action: {
-                        let alertHC = UIHostingController(rootView: MemberInputAlert(inputedMemberName: self.$inputedMemberName))
-                        alertHC.preferredContentSize = CGSize(width: 200, height: 100)
-                        alertHC.modalPresentationStyle = UIModalPresentationStyle.formSheet
-                        UIApplication.shared.windows[0].rootViewController?.present(alertHC, animated: true)
-                        self.isShowingModal.toggle()
-                    }, label: {
-                        Text("＋")
-                    })
-                    .frame(width:60, height:60)
-                    .background(Color.orange)
-                    .cornerRadius(30.0)
-                    .shadow(color: .gray, radius: 3, x: 3, y: 3)
-                    .padding(EdgeInsets(top: 0, leading: 0, bottom: 30.0, trailing: 30.0))
+                TextField("名前を入力", text: $inputedMemberName, onCommit: {
+                    if self.inputedMemberName == "" {
+                        return
+                    }
+                    self.inputedMemberName = ""
+                })
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding(EdgeInsets(top: 0, leading: 5, bottom: 30, trailing: 5))
+                Button(action:{
+                    self.appState.addMember(member: Member(name: self.inputedMemberName))
+                    self.inputedMemberName = ""
+                }) {
+                    ZStack(alignment: .center) {
+                        HStack(alignment: .center) {
+                            Text("追加")
+                                .foregroundColor(Color(red: 255/255, green: 255/255, blue: 255/255)) //white
+                                .font(Font.custom("Helvetica-Light", size: 20))
+                                .padding(EdgeInsets(top: 5, leading: 5, bottom: 5, trailing: 5))
+                        }
+                        .frame(minWidth: 0, maxWidth: 100, alignment: .center)
+                        .background(Color(red: 255/255, green: 127/255, blue: 80/255)) //coral
+                    }
+                    .cornerRadius(10)
+                    .shadow(color: Color(red: 173/255, green: 216/255, blue: 230/255), radius: 1, x: 0, y: 5) //lightblue
                 }
+                    .padding(EdgeInsets(top: 0, leading: 5, bottom: 30, trailing: 5))
             }
-            TextField("名前を入力", text: $inputedMemberName, onCommit: {
-                if self.inputedMemberName == "" {
-                    return
-                }
-                self.inputedMemberName = ""
-            })
-            .textFieldStyle(RoundedBorderTextFieldStyle())
-            .padding(EdgeInsets(top: 0, leading: 5, bottom: 30, trailing: 5))
         }
         .keyboardObserving()
         .background(Color(red: 77/255, green: 77/255, blue: 77/255)) //gray
-    }
-}
-
-struct MemberInputAlert: View {
-    @State private var name:String = ""
-    @Binding public var inputedMemberName:String
-
-    var body: some View {
-        ZStack() {
-            VStack() {
-                Text("名前を入力")
-                TextField("名前を入力", text: $name).textFieldStyle(RoundedBorderTextFieldStyle()).padding()
-                Divider()
-                HStack {
-                    Spacer()
-                    Button(action: {
-                        UIApplication.shared.windows[0].rootViewController?.dismiss(animated: true, completion: {
-                            self.inputedMemberName = self.name
-                        })
-                    }) {
-                        Text("OK")
-                    }
-                    Spacer()
-                    Divider()
-                    Spacer()
-                    Button(action: {
-                        UIApplication.shared.windows[0].rootViewController?.dismiss(animated: true, completion: {})
-                    }) {
-                        Text("Cancel")
-                    }
-                    Spacer()
-                }.padding(0)
-            }
-            .frame(width: 300, height: 200)
-            .background(Color(white: 0.9))
-        }
-        .background(Color.clear)
     }
 }
 
