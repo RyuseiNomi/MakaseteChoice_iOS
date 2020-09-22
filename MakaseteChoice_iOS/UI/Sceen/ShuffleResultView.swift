bc86e5047b201b11b89287e5d79d266a392f5e2d
RyuseiNomi <kanontonto1010@gmail.com>
2020-09-22 18:15:49 +0900

    cutout resultmember cell component to another file

--- MakaseteChoice_iOS/UI/Sceen/ShuffleResultView.swift
+++ MakaseteChoice_iOS/UI/Sceen/ShuffleResultView.swift
@@ -9,36 +9,6 @@
 import SwiftUI
 import QGrid
 
-struct ResultMemberCell: View {
-    var member: Member
-    
-    var body: some View {
-        ZStack() {
-            HStack() {
-                Image("Member")
-                Text(member.name)
-                    .foregroundColor(Color(red: 105/255, green: 105/255, blue: 105/255))
-                    .font(Font.custom("Helvetica-Light", size: 16))
-                Spacer()
-                VStack() {
-                    Text("グループ")
-                        .foregroundColor(Color(red: 105/255, green: 105/255, blue: 105/255))
-                        .font(Font.custom("Helvetica-Light", size: 12))
-                    Text(String(member.groupId))
-                        .foregroundColor(Color(red: 105/255, green: 105/255, blue: 105/255))
-                        .font(Font.custom("Helvetica", size: 20))
-                        .bold()
-                }
-                .padding(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing:5))
-            }
-            .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
-            .background(Color(red: 255/255, green: 255/255, blue: 255/255))
-        }
-        .cornerRadius(10)
-        .shadow(color: Color(red: 173/255, green: 216/255, blue: 230/255), radius: 1, x: 0, y: 5) //lightblue
-    }
-}
-
 struct ShuffleResultView: View {
     
     @State private(set) var groupNum:Int = 1
