77ae15dec0f05ba8248cc2b9f468ffff7e3e28d0
RyuseiNomi <kanontonto1010@gmail.com>
2020-09-23 00:44:19 +0900

    deleted shuffleActionButton and cutout shuffle option cell

--- MakaseteChoice_iOS/UI/Sceen/ShuffleOptionView.swift
+++ MakaseteChoice_iOS/UI/Sceen/ShuffleOptionView.swift
@@ -9,17 +9,32 @@
 import SwiftUI
 
 struct ShuffleOptionView: View {
-    
-    public var members:[Member] = []
-    @State private(set) var selectedNum = 0
-    @EnvironmentObject public var appState: AppState
 
+    @Binding public var groupNum: Int
+    @EnvironmentObject public var appState: AppState
+    
     var body: some View {
         VStack() {
-            Stepper(value: $selectedNum, in: 1...4) {
-                Text("\(selectedNum)")
+            //TODO 組分け数を決定するUIをモーダルなどに切り出す
+            HStack() {
+                Text("メンバー数")
+                    .foregroundColor(Color(red: 245/255, green: 245/255, blue: 245/255)) // whitesmoke
+                Text(String(self.appState.memberObject.members.count))
+                    .foregroundColor(Color(red: 245/255, green: 245/255, blue: 245/255)) //whitesmoke
+                    .font(Font.custom("Helvetica-Light", size: 16))
+                Text("人")
+                    .foregroundColor(Color(red: 245/255, green: 245/255, blue: 245/255)) // whitesmoke
+            }
+            HStack() {
+                if self.appState.memberObject.members.count != 0 {
+                    Text("組分け数")
+                        .foregroundColor(Color(red: 245/255, green: 245/255, blue: 245/255)) // whitesmoke
+                    Stepper(value: $groupNum, in: 1...self.appState.memberObject.members.count) {
+                        Text("\(groupNum)")
+                    }
+                }
             }
         }
-        .background(Color(red: 255/255, green: 250/255, blue: 240/255)) //floralwhite
+        .background(Color(red: 77/255, green: 77/255, blue: 77/255)) // gray
     }
 }
--- MakaseteChoice_iOS/UI/Sceen/ShuffleResultView.swift
+++ MakaseteChoice_iOS/UI/Sceen/ShuffleResultView.swift
@@ -38,25 +38,7 @@ struct ShuffleResultView: View {
             if self.appState.memberObject.members.count != 0 {
                 Spacer()
                 VStack() {
-                    //TODO 組分け数を決定するUIをモーダルなどに切り出す
-                    HStack() {
-                        Text("メンバー数")
-                            .foregroundColor(Color(red: 245/255, green: 245/255, blue: 245/255)) // whitesmoke
-                        Text(String(self.appState.memberObject.members.count))
-                            .foregroundColor(Color(red: 245/255, green: 245/255, blue: 245/255)) //whitesmoke
-                            .font(Font.custom("Helvetica-Light", size: 16))
-                        Text("人")
-                            .foregroundColor(Color(red: 245/255, green: 245/255, blue: 245/255)) // whitesmoke
-                    }
-                    HStack() {
-                        if self.appState.memberObject.members.count != 0 {
-                            Text("組分け数")
-                                .foregroundColor(Color(red: 245/255, green: 245/255, blue: 245/255)) // whitesmoke
-                            Stepper(value: $groupNum, in: 1...self.appState.memberObject.members.count) {
-                                Text("\(groupNum)")
-                            }
-                        }
-                    }
+                    ShuffleOptionView(groupNum: self.$groupNum)
                     Button(action: {
                         self.doShuffle()
                     }) {
