9534272b2c6f5d81b22068d6bc29a38acc1f1e41
RyuseiNomi <kanontonto1010@gmail.com>
2020-09-23 19:52:13 +0900

    add optional integer to process to detect if pinned

--- MakaseteChoice_iOS/UI/Component/ResultMemberCell.swift
+++ MakaseteChoice_iOS/UI/Component/ResultMemberCell.swift
@@ -45,8 +45,15 @@ struct ResultMemberCell: View {
     
     /// ピン留めの対象となるメンバーの現在のピン留めステータスを取得
     private func getMemberPinnedStatus() -> Bool {
-        let targetMemberIndex = self.appState.memberObject.members.index(where: { $0.name == member.name })
-        return self.appState.memberObject.members[targetMemberIndex!].isPinned
+        let targetMemberIndex:Int? = self.appState.memberObject.members.index(where: { $0.name == member.name })
+        // ピン留めしたユーザが既に削除されている場合はfalseを返す
+        // Optional型の恩恵
+        if targetMemberIndex == nil {
+            print("削除されたユーザを選択")
+            return false
+        }
+        let targetMember: Member = self.appState.memberObject.members[targetMemberIndex!]
+        return targetMember.isPinned
     }
 }
 
