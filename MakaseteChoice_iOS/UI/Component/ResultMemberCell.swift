a8557382601d9766b587692f39301d28952da32f
RyuseiNomi <kanontonto1010@gmail.com>
2020-09-23 17:01:27 +0900

    minior midification for MemberCell

--- MakaseteChoice_iOS/UI/Component/ResultMemberCell.swift
+++ MakaseteChoice_iOS/UI/Component/ResultMemberCell.swift
@@ -12,29 +12,26 @@ struct ResultMemberCell: View {
     var member: Member
     
     var body: some View {
-        ZStack() {
+        HStack() {
+            Text(String(member.groupId))
+                .foregroundColor(Color(red: 255/255, green: 255/255, blue: 255/255))
+                .font(Font.custom("Helvetica", size: 30))
+                .fontWeight(.black)
+                .padding(EdgeInsets(top: 5, leading: 5, bottom: 5, trailing: 15))
             HStack() {
                 Image("Member")
                 Text(member.name)
                     .foregroundColor(Color(red: 105/255, green: 105/255, blue: 105/255))
-                    .font(Font.custom("Helvetica-Light", size: 16))
+                    .font(Font.custom("Helvetica-Light", size: 20))
                 Spacer()
-                VStack() {
-                    Text("グループ")
-                        .foregroundColor(Color(red: 105/255, green: 105/255, blue: 105/255))
-                        .font(Font.custom("Helvetica-Light", size: 12))
-                    Text(String(member.groupId))
-                        .foregroundColor(Color(red: 105/255, green: 105/255, blue: 105/255))
-                        .font(Font.custom("Helvetica", size: 20))
-                        .bold()
-                }
-                .padding(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing:5))
+                Image(systemName: "pin")
+                    .padding(EdgeInsets(top: 5, leading: 5, bottom: 5, trailing: 15))
             }
             .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
             .background(Color(red: 255/255, green: 255/255, blue: 255/255))
-        }
-        .cornerRadius(10)
+            .cornerRadius(10)
             .shadow(color: Color(red: 173/255, green: 216/255, blue: 230/255), radius: 1, x: 0, y: 5) //lightblue
+        }
     }
 }
 
