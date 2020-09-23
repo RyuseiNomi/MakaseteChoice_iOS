5c478ef261e6b50156531b19b6d24e53a584c371
RyuseiNomi <kanontonto1010@gmail.com>
2020-09-24 00:28:05 +0900

    [WIP] cutout Picker modal to ShuffleResultView

--- MakaseteChoice_iOS/UI/Sceen/ShuffleResultView.swift
+++ MakaseteChoice_iOS/UI/Sceen/ShuffleResultView.swift
@@ -15,47 +15,122 @@ struct ShuffleResultView: View {
     @State var group:[Member] = []
     @State private(set) var isCompletShuffle = false
     @State private var isShowingModal = false
+    // Pickerのポジションに関するState変数群
+    @State public var currentOffset = CGFloat.zero
+    @State public var openOffset = CGFloat.zero
+    @State public var closeOffset = CGFloat.zero
     @EnvironmentObject public var appState: AppState
 
     var body: some View {
+        GeometryReader { geometry in
+            VStack() {
+                HStack() {
+                    Text("チョイス結果")
+                        .fontWeight(.black)
+                        .foregroundColor(Color(red: 245/255, green: 245/255, blue: 245/255)) //whitesmoke
+                        .font(Font.custom("Helvetica-Light", size: 25))
+                        .padding(EdgeInsets(top: 10, leading: 5, bottom: 10, trailing: 0))
+                    Spacer()
+                    Text("メンバー数: \(self.group.count)")
+                        .foregroundColor(Color(red: 245/255, green: 245/255, blue: 245/255)) //whitesmoke
+                        .font(Font.custom("Helvetica-Light", size: 15))
+                        .padding(EdgeInsets(top: 10, leading: 0, bottom: 5, trailing: 5))
+                }
+                .frame(maxWidth: .infinity, maxHeight: 50, alignment: .leading)
+                .padding(EdgeInsets(top: 20, leading: 10, bottom: 10, trailing: 0))
+                if self.appState.memberObject.members.count == 0 {
+                    NoMemberViewComponent(paragraphOne: "メンバーがいません", paragraphTwo: "「メンバー」メニューから", paragraphThree: "メンバーを追加しましょう")
+                } else if self.isCompletShuffle == false {
+                    NoGroupViewComponent(paragraphOne: "グループがありません", paragraphTwo: "組分け数を設定して", paragraphThree: "グループをチョイスしましょう")
+                } else {
+                    GroupCell(members: self.group)
+                }
+                if self.appState.memberObject.members.count != 0 {
+                    Spacer()
+                    VStack() {
+                        ShuffleOptionView(
+                            groupNum: self.$groupNum,
+                            currentOffset: self.$currentOffset,
+                            openOffset: self.$openOffset,
+                            closeOffset: self.$closeOffset
+                        )
+                        Button(action: {
+                            let shuffleInteractor = ShuffleInteractor(appState: self.appState)
+                            self.group = shuffleInteractor.doShuffle(groupNumber: self.groupNum)
+                            self.isCompletShuffle = true
+                        }) {
+                            DecisionButton(label: "チョイスする", maxWidth: 200)
+                        }
+                        .padding(EdgeInsets(top: 15, leading: 5, bottom: 30, trailing: 5))
+                    }
+                }
+            }
+            .background(Color(red: 77/255, green: 77/255, blue: 77/255)) // gray
+            GroupNumPicker(
+                membersCount: self.appState.memberObject.members.count,
+                viewHeight: geometry.size.height,
+                groupNum: self.$groupNum,
+                currentOffset: self.$currentOffset,
+                openOffset: self.$openOffset,
+                closeOffset: self.$currentOffset
+            )
+                .frame(height: geometry.size.height * 0.5)
+                .onAppear(perform: {
+                    self.setInitPosition(viewHeight: geometry.size.height)
+                })
+                .offset(y: self.currentOffset)
+                .animation(.default)
+        }
+    }
+    
+    // NumberPickerを画面の下に隠すために初期ポジションを設定する
+    private func setInitPosition(viewHeight: CGFloat) {
+        self.currentOffset = viewHeight
+        self.closeOffset = self.currentOffset
+        self.openOffset = viewHeight - viewHeight*0.5
+    }
+    
+    // NumberPickerの座標を書き換えることで出現させる
+    private func toggleNumberPicker() {
+        self.currentOffset = self.openOffset
+    }
+}
+
+struct GroupNumPicker: View {
+    
+    public var membersCount:Int
+    public var viewHeight:CGFloat
+    @Binding public var groupNum:Int
+    // Pickerのポジションに関するState変数群
+    @Binding public var currentOffset:CGFloat
+    @Binding public var openOffset:CGFloat
+    @Binding public var closeOffset:CGFloat
+    
+    var body: some View {
         VStack() {
             HStack() {
-                Text("チョイス結果")
-                    .fontWeight(.black)
-                    .foregroundColor(Color(red: 245/255, green: 245/255, blue: 245/255)) //whitesmoke
-                    .font(Font.custom("Helvetica-Light", size: 25))
-                    .padding(EdgeInsets(top: 10, leading: 5, bottom: 10, trailing: 0))
-                Spacer()
-                Text("メンバー数: \(self.group.count)")
-                    .foregroundColor(Color(red: 245/255, green: 245/255, blue: 245/255)) //whitesmoke
-                    .font(Font.custom("Helvetica-Light", size: 15))
-                    .padding(EdgeInsets(top: 10, leading: 0, bottom: 5, trailing: 5))
+                Text("組分け数を選択")
+                    .foregroundColor(Color(red: 255/255, green: 255/255, blue: 255/255))
+                    .font(Font.custom("Helvetica-Light", size: 12))
+                Button(action: {
+                    self.currentOffset = self.viewHeight
+                }) {
+                    Text("完了")
+                }
             }
-            .frame(maxWidth: .infinity, maxHeight: 50, alignment: .leading)
-            .padding(EdgeInsets(top: 20, leading: 10, bottom: 10, trailing: 0))
-            if self.appState.memberObject.members.count == 0 {
-                NoMemberViewComponent(paragraphOne: "メンバーがいません", paragraphTwo: "「メンバー」メニューから", paragraphThree: "メンバーを追加しましょう")
-            } else if isCompletShuffle == false {
-                NoGroupViewComponent(paragraphOne: "グループがありません", paragraphTwo: "組分け数を設定して", paragraphThree: "グループをチョイスしましょう")
-            } else {
-                GroupCell(members: self.group)
-            }
-            if self.appState.memberObject.members.count != 0 {
-                Spacer()
-                VStack() {
-                    ShuffleOptionView(groupNum: self.$groupNum)
-                    Button(action: {
-                        let shuffleInteractor = ShuffleInteractor(appState: self.appState)
-                        self.group = shuffleInteractor.doShuffle(groupNumber: self.groupNum)
-                        self.isCompletShuffle = true
-                    }) {
-                        DecisionButton(label: "チョイスする", maxWidth: 200)
-                    }
-                    .padding(EdgeInsets(top: 15, leading: 5, bottom: 30, trailing: 5))
+            Picker(selection: self.$groupNum, label: Text("")) {
+                ForEach(1..<self.membersCount) {
+                    Text("\($0)")
                 }
             }
         }
-        .background(Color(red: 77/255, green: 77/255, blue: 77/255)) // gray
+        .frame(maxWidth: .infinity, maxHeight: 300, alignment: .center)
+        .background(Color.white)
+    }
+    
+    // NumberPickerの座標を書き換えることで出現させる
+    private func toggleNumberPicker() {
+        self.currentOffset = self.closeOffset
     }
 }
 
