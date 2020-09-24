4bbc01248d845fe4f817cf78829839fb8cb7143a
RyuseiNomi <kanontonto1010@gmail.com>
2020-09-24 14:07:52 +0900

    modify credential view

--- MakaseteChoice_iOS/UI/Sceen/CredentialView.swift
+++ MakaseteChoice_iOS/UI/Sceen/CredentialView.swift
@@ -11,26 +11,44 @@ import SwiftUI
 struct CredentialView: View {
     
     private let icon8Url = URL(string: "https://icons8.com")!
+    private let privacyPolicyUrl = URL(string: "https://ryuseinomi.github.io/MakaseteChoice_iOS/")!
     
     var body: some View {
         VStack(alignment: .leading) {
-            Text("アプリのバージョン")
+            Text("App version")
                 .foregroundColor(Color(red: 245/255, green: 245/255, blue: 245/255)) // whitesmoke
-                .font(Font.custom("Helvetica-Light", size: 24))
-                .padding()
-            Text("1.1.0")
-                .foregroundColor(Color(red: 245/255, green: 245/255, blue: 245/255)) // whitesmoke
-                .padding()
-            Divider()
+                .font(Font.custom("Helvetica-Light", size: 20))
+                .fontWeight(.black)
+                .padding(EdgeInsets(top: 10, leading: 20, bottom: 10, trailing: 0))
+            Text("2.0.0")
+                .foregroundColor(Color(red: 255/255, green: 255/255, blue: 255/255))
+                .font(Font.custom("Helvetica", size: 20))
+                .padding(EdgeInsets(top: 10, leading: 20, bottom: 20, trailing: 0))
             Text("Credit")
                 .foregroundColor(Color(red: 245/255, green: 245/255, blue: 245/255)) // whitesmoke
-                .font(Font.custom("Helvetica-Light", size: 24))
-                .padding()
+                .font(Font.custom("Helvetica-Light", size: 20))
+                .fontWeight(.black)
+                .padding(EdgeInsets(top: 10, leading: 20, bottom: 10, trailing: 0))
             HStack() {
                 Text("Icons in this app by ")
                     .foregroundColor(Color(red: 245/255, green: 245/255, blue: 245/255)) // whitesmoke
                 Button(action : { UIApplication.shared.open(self.icon8Url) }) {
                     Text("Icons8")
+                        .foregroundColor(Color(red: 245/255, green: 245/255, blue: 245/255)) // whitesmoke
+                        .underline()
+                }
+            }
+            .padding(EdgeInsets(top: 10, leading: 20, bottom: 10, trailing: 0))
+            Text("PrivacyPolicy")
+                .foregroundColor(Color(red: 245/255, green: 245/255, blue: 245/255)) // whitesmoke
+                .font(Font.custom("Helvetica-Light", size: 20))
+                .fontWeight(.black)
+                .padding()
+            HStack() {
+                Text("This app has a ")
+                    .foregroundColor(Color(red: 245/255, green: 245/255, blue: 245/255)) // whitesmoke
+                Button(action : { UIApplication.shared.open(self.privacyPolicyUrl) }) {
+                    Text("privacy policy")
                         .foregroundColor(Color(red: 245/255, green: 245/255, blue: 245/255)) // whitesmoke
                         .underline()
                 }
